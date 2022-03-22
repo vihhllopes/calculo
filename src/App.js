3291b7f3deb48abc983707a4802f9d3dc3ede8b0
victoria <viictoria931@gmail.com>
2022-03-22 12:20:58 -0300

    calculo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+  const [idade, setIdade] = useState();
+  const handleIdadeChange = (e) => {
+    setIdade(e.target.value);
+  }
+
+  const [checked, setChecked] = useState(false);
+  const now = new Date();
+  const anoAtual = now.getFullYear();
+  const [block, setBlock] = useState(false);
+  const [ano, setAno] = useState();
+  
+  const handleBtCalculaClick = () => {
+    var calculoAno = checked ? (anoAtual - idade)  :  (anoAtual - idade) - 1
+    setAno(calculoAno)
+    handleBtBlock()
+  
+  }
+
+  const handleBtBlock = () => {
+    setBlock(!block)
+  }
+
+  const handleBtLimparClick = () => {
+    setAno(0)
+    setIdade(0)
+    setChecked(!checked)
+    handleBtBlock()
+  }
+
+  const handleBtChecked = () => {
+    setChecked(!checked)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="box">
+      <h1>Cálculo do Ano de Nascimento</h1>
+      <p>
+      <label for="fname"> Informe sua Idade: </label>
+      <input value={idade} readOnly={block} type="number" onChange={handleIdadeChange} />
+      </p>
+      <p>Já fez aniversário esse ano? <br />
+      <a>SIM</a>
+        <input type="checkbox" onChange={handleBtChecked} value={checked} checked={checked} /> <br/>
+      </p>
+
+      <button class='button' onClick={handleBtCalculaClick}>Calcular</button>
+      <button class='button' onClick={handleBtLimparClick}>Limpar</button>
+
+      <p>Seu Ano de Nascimento:<br />
+        <input readOnly value={ano} />
+      </p>
+
     </div>
   );
 }
